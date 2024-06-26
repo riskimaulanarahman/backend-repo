8f5ba66f23e0c07efa9cc91413e5d5d670db2644
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2024-06-26 11:53:28 +0800

    fix : hide databaseURL

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -4,10 +4,9 @@ const serviceAccount = require('../credentials/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ebuddy-1f743-default-rtdb.firebaseio.com"
+  databaseURL: "https://databases.firebaseio.com"
 });
 
 const db = admin.firestore();
 
-// export default db;
 export { db, admin };
